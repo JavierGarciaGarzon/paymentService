48b371797d92cca675dee9bc505dd148ebf08146
jggarzon <javiergarciagarzon@gmail.com>
2024-09-12 15:39:12 +0200

    Add openApi config

---
+++ payment/src/main/java/com/nttdata/payment/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.nttdata.payment.infrastructure.config;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Payment Service API")
+                        .version("1.0")
+                        .description("API for managing credit card payments"));
+    }
+}
